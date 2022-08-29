10bd001434c403f2e35ebc978e9dee50a53eea5f
TommasoRaimondi <raimondi.tommaso21@gmail.com>
2022-08-29 09:43:22 +0200

    Aggiunta estetica console +resoconto lanci

--- js/script.js
+++ js/script.js
@@ -2,10 +2,14 @@
 
 let throwNum = 0;
 
+let dice1Wins = 0;
+let dice2Wins = 0;
+
 throwNum = parseInt(prompt('Inserire numero di lanci da effettuare:'));
 
 for (let i = 0; i < throwNum; i++) {
 
+    console.log('---------------')
     console.log('Numero lancio: ' + (i + 1));
 
     const dice1 = Math.floor((Math.random() * 6) + 1);
@@ -16,10 +20,17 @@ for (let i = 0; i < throwNum; i++) {
 
     if (dice1 > dice2) {
         console.log('Vince Dado 1!');
+        dice1Wins++;
     } else if (dice2 > dice1) {
         console.log('Vince Dado 2!');
+        dice2Wins++;
     } else {
         console.log('Pareggio!');
     }
 
 }
+
+console.log('---------------');
+console.log(`Numero vittorie Dado 1: ${dice1Wins}`);
+console.log(`Numero vittorie Dado 2: ${dice2Wins}`);
+console.log(`Numero pareggi: ${throwNum - dice1Wins - dice2Wins}`);
